95743029853972d7fc9631c1c84c9d2f2fe25e08
Rohit Chaudhary <65174148+rohitchaudhary-mnnit@users.noreply.github.com>
2022-07-04 00:07:40 +0545

    3rd day (After Break)

---
+++ Sort012.java
@@ -0,0 +1,31 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+    public static void sort012(int[] arr)
+    {
+        //Write your code here
+        int n = arr.length;
+        int lo = 0, mid = 0;
+        int hi = n - 1;
+        
+        while(mid <= hi) {
+            if(arr[mid] == 0) {
+                int temp = arr[lo];
+                arr[lo] = arr[mid];
+                arr[mid] = temp;
+                lo++;
+                mid++;
+            }
+            else if(arr[mid] == 1) {
+                mid++;
+            }
+            else {
+                int temp = arr[mid];
+                arr[mid] = arr[hi];
+                arr[hi] = temp;
+                hi--;
+            }
+        }
+    }
+}
